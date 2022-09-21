4434dcd4a648122542c2da23ea3f24a347ceef0c
Riley-jpg <carlson.riley24@libertycommon.org>
2022-09-21 16:09:54 -0700

    hw map started and mecanum drive code created

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/babyHwMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/babyHwMap.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class babyHwMap {
+
+public class babyHwMap extends HardwareMapUtil {
+
+    HardwareMap hwmap = null;
+
+    /* Public OpMode members. */
+    public DcMotor leftfrontDrive = null;
+    public DcMotor rightfrontDrive = null;
+    public DcMotor leftbackDrive = null;
+    public DcMotor rightbackDrive = null;
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+        leftfrontDrive = HardwareInitMotor("lfD", true);
+        rightfrontDrive = HardwareInitMotor ("rfD", false);
+        leftbackDrive = HardwareInitMotor ("lbD", true);
+        rightbackDrive = HardwareInitMotor ("rbD", false);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/babyTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/babyTeleop.java
@@ -1,4 +1,53 @@
 package org.firstinspires.ftc.teamcode;
+import android.graphics.Color;
 
-public class babyTeleop {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp (name= "babyTeleop", group= "Pushbot")
+
+public class babyTeleop extends LinearOpMode {
+
+    babyHwMap robot = new babyHwMap();
+    private ElapsedTime runtime = new ElapsedTime();
+    static double turnPower = 0.5;
+    static double fwdBackPower = 0.5;
+    static double strafePower = 0.5;
+    static double lbPower;
+    static double lfPower;
+    static double rbPower;
+    static double rfPower;
+    static double slowamount = 1;
+
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            fwdBackPower = gamepad1.left_stick_y;
+            strafePower = gamepad1.left_stick_x;
+            turnPower = -gamepad1.right_stick_x;
+
+            lfPower = fwdBackPower - turnPower - strafePower;
+            rfPower = fwdBackPower + turnPower + strafePower;
+            lbPower = fwdBackPower - turnPower + strafePower;
+            rbPower = fwdBackPower + turnPower - strafePower;
+
+            robot.leftfrontDrive.setPower(-lfPower*slowamount);
+            robot.leftbackDrive.setPower(-lbPower*slowamount);
+            robot.rightfrontDrive.setPower(-rfPower*slowamount);
+            robot.rightbackDrive.setPower(-rbPower*slowamount);
+
+        }
+    }
 }
