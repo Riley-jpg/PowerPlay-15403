8c5b6c1309a73a4a811ec7c97588ee36a74d310a
Riley-jpg <carlson.riley24@libertycommon.org>
2023-01-19 15:59:59 -0700

    setup stuff for new robot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/otherTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/otherTeleop.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -12,8 +13,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp (name= "other teleop", group= "Pushbot")
 public class otherTeleop extends otherHwMap{
+
+    //speed variables here!!
+
     otherHwMap robot = new otherHwMap();
     private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode(){
+
+    }
 
 
 
