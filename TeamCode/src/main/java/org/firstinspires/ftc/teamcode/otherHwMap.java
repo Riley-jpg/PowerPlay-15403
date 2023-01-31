09bc43fafa6c1c1c897c33794037b399eb7d5d49
vinpe75 <vinpe75@libertycommon.org>
2023-01-31 16:37:51 -0700

    new robot code :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/otherHwMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/otherHwMap.java
@@ -9,4 +9,35 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class otherHwMap extends HardwareMapUtil{
     HardwareMap hwmap =null;
+
+    public DcMotor leftfrontDrive = null;
+    public DcMotor rightfrontDrive = null;
+    public DcMotor leftbackDrive = null;
+    public DcMotor rightbackDrive = null;
+    public DcMotor linearActuator = null;
+    public DcMotor linearActuator2 = null;
+    public Servo openServo = null;
+
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+        leftfrontDrive = HardwareInitMotor("lfD", true);
+        rightfrontDrive = HardwareInitMotor("rfD", true);
+        leftbackDrive = HardwareInitMotor("lbD", true);
+        rightbackDrive = HardwareInitMotor("rbD", true);
+
+        linearActuator = HardwareInitMotor("la1", true);
+        linearActuator2 = HardwareInitMotor("la2", true);
+
+        openServo = hwMap.get(Servo.class,"os");
+
+
+        leftfrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightfrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftbackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightbackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linearActuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linearActuator2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
 }
